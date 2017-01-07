40f52523e5e93ebfd600931fd808a9a1ea5e6d17
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-06 18:56:17 -0800

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO  `tech2u`.`SmartForms` (
+  `Name` ,
+  `ReadFields` ,
+  `WriteFields` ,
+  `InsertedTime` ,
+  `InsertedUser`
+)
+VALUES (
+  'example',  
+  'a,b',  
+  'a,b,c,d', 
+  NOW( ) ,  
+  '1'
+);
